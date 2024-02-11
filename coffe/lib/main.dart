84e8959f7ff5108d8a86cb6670f201088188de6a
Shamindi <ngdilshan2002@gmail.com>
2024-02-12 00:32:42 +0530

    Set Up Project
    Set Environment And Main Page

---
+++ coffe/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:coffe/screens/WelcomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Color(0x397B5F)),
+      home: WelcomeScreen(),
+    );
+  }
+}
