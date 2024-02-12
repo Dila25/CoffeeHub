099252bca6a42e091d1d766e151ae8ed32fa4951
Shamindi <ngdilshan2002@gmail.com>
2024-02-12 20:58:32 +0530

    hh

--- coffe/lib/screens/HomeScreen.dart
+++ coffe/lib/screens/HomeScreen.dart
@@ -7,8 +7,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+    _tabController.addListener(_handleTabSelection);
+    super.initState();
+  }
+
+  _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.only(top: 15),
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
